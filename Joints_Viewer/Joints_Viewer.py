e6b1928bb781e6a805f788eea749e931665d5e7d
Gigi-G <seminara.luigi@gmail.com>
2021-05-07 03:19:27 +0200

    fix&feat: Joints_Viewer.py

--- Joints_Viewer/Joints_Viewer.py
+++ Joints_Viewer/Joints_Viewer.py
@@ -1,30 +1,11 @@
-import queue
 import sys
 from PyQt5 import QtWidgets
 from modules.VideoWindow import VideoWindow
-from modules.GraphWindow import GraphWindow
-from threading import Semaphore, Thread
-from queue import Queue
-
-queue_data:Queue = Queue(1)
-full:Semaphore = Semaphore(0)
-empty:Semaphore = Semaphore(1)
-access:Semaphore = Semaphore(1)
-
-def video_window():
-    player.show()
-
-def graph_view():
-    graph.show()
+from modules.Plot3DWindow import Plot3DWindow
 
 if __name__ == '__main__':
     app:QtWidgets.QApplication = QtWidgets.QApplication(sys.argv)
-    player:VideoWindow = VideoWindow(GraphWindow())
+    player:VideoWindow = VideoWindow(Plot3DWindow())
     player.resize(640, 480)
-    graph:GraphWindow = GraphWindow()
-    graph.resize(640, 480)
-    #thread_video = Thread(target=video_window)
-    thread_graph = Thread(target=graph_view)
-    #thread_video.start()
-    thread_graph.start()
+    player.show()
     sys.exit(app.exec_())
