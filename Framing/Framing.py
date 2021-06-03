b0f598012d3af85d9299634d8ed8fb650691dc17
Gigi-G <seminara.luigi@gmail.com>
2021-06-03 19:05:35 +0200

    feat: Framing script
    - DataFrame creation using all data

--- Framing/Framing.py
+++ Framing/Framing.py
@@ -2,7 +2,7 @@ from modules.joints_dataframe import JointsDataFrame
 
 def main() -> None:
     dataframe:JointsDataFrame = JointsDataFrame()
-    print(dataframe.get_dataframe())
+    dataframe.get_dataframe().to_csv("./framing_action.csv", index=False)
 
 if __name__ == "__main__":
     main()
--- Framing/modules/joints_dataframe.py
+++ Framing/modules/joints_dataframe.py
@@ -3,6 +3,7 @@ from modules.hand_pose import HandPose
 import glob
 import json
 import pandas as pd
+from modules.target import Target
 
 class JointsDataFrame():
 
@@ -17,7 +18,7 @@ class JointsDataFrame():
         self.__videos:list = glob.glob("../data/Video/*.mp4")
         self.__annotations:list = glob.glob("../data/Annotation/*.json")
         self.__hand_poses:list = glob.glob("../data/HandPoses/*.txt")
-        self.__actions:list = glob.glob("../data/Actions/*.txt")
+        #self.__actions:list = glob.glob("../data/Actions/*.txt")
 
     def __load_hand_joints(self) -> None:
         with open("./hand_joints.json") as f:
@@ -29,16 +30,23 @@ class JointsDataFrame():
         frame_rate:int = 30
         return 1000/frame_rate
 
-    def __create_records(self, video_duration:int, start_tim:int, hand_pose_dict:dict, data:dict) -> None:
+    def __create_records(self, video_duration:int, start_tim:int, hand_pose_dict:dict, data:dict, target:dict) -> None:
         while video_duration > 0:
             index:int = 0
             for key in data:
-                data[key].append(0)
+                if key == "TARGET":
+                    data[key].append("No_action")
+                else:
+                    data[key].append(0)
             video_duration -= self.__win_size()
             while index < (self.__win_size() if video_duration >=0 else self.__win_size() + video_duration):
                 if str(start_tim) in hand_pose_dict:
                     for key, val in hand_pose_dict[str(start_tim)].items():
                         data[key][self.__index_data] = val
+                    for _, val in target.items():
+                        if val["time"][0] <= start_tim <= val["time"][1]:
+                            data["TARGET"][self.__index_data] = val["action"]
+                            break
                 start_tim += 1
                 index += 1
             self.__index_data += 1
@@ -52,6 +60,7 @@ class JointsDataFrame():
                 video_duration,
                 hand_pose.get_start_time(),
                 hand_pose.get_hand_pose_dict(),
-                records
+                records,
+                Target.get(self.__annotations[self.__videos.index(video)], hand_pose.get_start_time())
             )
         return pd.DataFrame(records, columns=self.__hand_joint_names)
