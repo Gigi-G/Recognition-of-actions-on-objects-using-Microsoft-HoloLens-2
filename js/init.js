4e3d2b023d1c514619b5c7369fbdb02a112e703d
Gigi-G <seminara.luigi@gmail.com>
2021-10-29 23:41:59 +0200

    chore: new site

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+  
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+})(jQuery); // end of jQuery name space
